33a3d4cd379aef87fd7f7117d3f37dc4ea2060a5
Raymond T. Hightower <rth@wisdomgroup.com>
2013-10-21 10:24:14 -0500

    initial commit

---
+++ websvr.js
@@ -0,0 +1,9 @@
+// Sample web server from http://nodejs.org
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World. This is my first Node.js program!!!\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
