b4cff59dc281c6918a93f23541c2bcc66c68d6b4
Raymond T. Hightower <rth@wisdomgroup.com>
2014-06-24 00:59:43 -0500

    line two

--- websvr.js
+++ websvr.js
@@ -4,6 +4,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World. This is my first Node.js program!!!\n');
+  res.end('Line two...\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
