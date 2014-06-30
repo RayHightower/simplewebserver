e051bb699308247a3d1dac4c2ab2d37728297db7
Raymond T. Hightower <rth@wisdomgroup.com>
2014-06-30 15:40:13 -0500

    just playing around...

--- websvr.js
+++ websvr.js
@@ -1,9 +1,9 @@
-// Sample web server from http://nodejs.org
+// Sample web server from http://nodejs.org, slightly modified.
 
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World. This is my first Node.js program!!!\nLine two...');
+  res.end('Hello World. OpenROV runs Node.js!');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
