9c1bf364cd1a5ced44c8d9b0b917217909f7f7ea
Raymond T. Hightower <rth@wisdomgroup.com>
2014-09-12 21:52:19 -0500

    random experimenting

--- websvr.js
+++ websvr.js
@@ -3,7 +3,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World. OpenROV runs Node.js!');
+  res.end('Hello World. OpenROV runs Node.js!\nHow are escape characters handled?');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
