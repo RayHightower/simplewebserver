54557a8d17a1a0049753f12909389d439e03ab9d
Raymond T. Hightower <rth@wisdomgroup.com>
2014-10-16 17:10:28 -0500

    goofing around to see how stuff works

--- websvr.js
+++ websvr.js
@@ -3,7 +3,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World. OpenROV runs Node.js!\nHow are escape characters handled?');
+  res.end('Hello World. OpenROV runs Node.js!\nHow are escape characters handled?\nOh... Like that!');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
