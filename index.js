7f29e317d90302abea435ee96940c478d6b589fe
Raymond T. Hightower <rth@wisdomgroup.com>
2014-10-16 17:36:57 -0500

    it works!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.sendFile('./index.html');
+  res.sendfile('index.html');
 });
 
 http.listen(3000, function(){
