6920027fd4f322b9f8fd9f2796ac2fd7efc8b5b2
Raymond T. Hightower <rth@wisdomgroup.com>
2014-10-17 18:03:44 -0500

    before the next round of experiments

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
